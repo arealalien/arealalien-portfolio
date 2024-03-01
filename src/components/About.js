bb9dce99ce0c3cd5a929c60581bf0f7e9591626f
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2024-03-01 21:49:59 +0100

    started on about

--- src/components/About.js
+++ src/components/About.js
@@ -5,9 +5,16 @@ const About = () => {
         <section className='about'>
             <div className='about-inner component-grid view-width'>
                 <div className='component-grid-left'>
-                    <h2 className='component-grid-title'>/About</h2>
+                    <div className='about-inner-left'>
+                        <h2 className='component-grid-title'>/About Me</h2>
+                        <h3 className='about-inner-left-title'>I craft engaging digital experiences that drive results</h3>
+                        <button className='about-inner-left-button'>Learn More</button>
+                    </div>
                 </div>
                 <div className='component-grid-right'>
+                    <div className='about-inner-video'>
+
+                    </div>
                 </div>
             </div>
         </section>
