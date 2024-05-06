c398f8a095f4e430419b1438c0be97e68c125460
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2024-05-06 19:50:48 +0200

    added contact

--- src/components/ContactSec.js
+++ src/components/ContactSec.js
@@ -6,9 +6,11 @@ const Contact = () => {
             <section className='contact'>
                 <div className='contact-inner component-grid view-width'>
                     <div className='component-grid-top'>
-                        <h2 className='component-grid-title'>/Let's talk/</h2>
+                        <h2 className='component-grid-title cgt'>/Let's talk/</h2>
                     </div>
                     <div className='component-grid-bottom'>
+                        <h1>Ready to build experiences your audience will love?</h1>
+                        <button>Let's talk</button>
                     </div>
                 </div>
             </section>
