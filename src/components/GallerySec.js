b069945e8bc9a96c5b43173c552daf33a1b7aeac
dlirA01 ♡ <arild.edvin.andreassen@gmail.com>
2024-05-14 21:45:16 +0200

    update to gallery

--- src/components/GallerySec.js
+++ src/components/GallerySec.js
@@ -10,19 +10,49 @@ const Gallery = () => {
                             <h2 className='component-grid-title'>/Gallery</h2>
                         </div>
                     </div>
-                    <div className='component-grid-right view-width'>
+                    <div className='component-grid-right'>
                         <div className='gallery-inner-showcase'>
-                            <div className='gallery-inner-showcase-item'>
-                                <img className='gallery-inner-showcase-item-background'
-                                     src={process.env.PUBLIC_URL + `/images/gallery/image-6.jpg`}/>
-                            </div>
-                            <div className='gallery-inner-showcase-item'>
-                                <img className='gallery-inner-showcase-item-background'
-                                     src={process.env.PUBLIC_URL + `/images/gallery/image-8.jpg`}/>
-                            </div>
-                            <div className='gallery-inner-showcase-item'>
-                                <img className='gallery-inner-showcase-item-background'
-                                     src={process.env.PUBLIC_URL + `/images/gallery/image-2.jpg`}/>
+                            <div className='gallery-inner-showcase-glider'>
+                                <div className='gallery-inner-showcase-item'>
+                                    <div className='gallery-inner-showcase-item-info'>
+                                        <p className='gallery-inner-showcase-item-info-date'>14.05.2024</p>
+                                        <h3 className='gallery-inner-showcase-item-info-title'>Orange Bird</h3>
+                                        <button className='gallery-inner-showcase-item-info-button'>Open Gallery
+                                        </button>
+                                    </div>
+                                    <img className='gallery-inner-showcase-item-background'
+                                         src={process.env.PUBLIC_URL + `/images/gallery/image-6.jpg`}/>
+                                </div>
+                                <div className='gallery-inner-showcase-item'>
+                                    <div className='gallery-inner-showcase-item-info'>
+                                        <p className='gallery-inner-showcase-item-info-date'>14.05.2024</p>
+                                        <h3 className='gallery-inner-showcase-item-info-title'>Snow Bird</h3>
+                                        <button className='gallery-inner-showcase-item-info-button'>Open Gallery
+                                        </button>
+                                    </div>
+                                    <img className='gallery-inner-showcase-item-background'
+                                         src={process.env.PUBLIC_URL + `/images/gallery/image-8.jpg`}/>
+                                </div>
+                                <div className='gallery-inner-showcase-item'>
+                                    <div className='gallery-inner-showcase-item-info'>
+                                        <p className='gallery-inner-showcase-item-info-date'>14.05.2024</p>
+                                        <h3 className='gallery-inner-showcase-item-info-title'>White Bird</h3>
+                                        <button className='gallery-inner-showcase-item-info-button'>Open Gallery
+                                        </button>
+                                    </div>
+                                    <img className='gallery-inner-showcase-item-background'
+                                         src={process.env.PUBLIC_URL + `/images/gallery/image-2.jpg`}/>
+                                </div>
+                                <div className='gallery-inner-showcase-item'>
+                                    <div className='gallery-inner-showcase-item-info'>
+                                        <p className='gallery-inner-showcase-item-info-date'>14.05.2024</p>
+                                        <h3 className='gallery-inner-showcase-item-info-title'>Snow Bird</h3>
+                                        <button className='gallery-inner-showcase-item-info-button'>Open Gallery
+                                        </button>
+                                    </div>
+                                    <img className='gallery-inner-showcase-item-background'
+                                         src={process.env.PUBLIC_URL + `/images/gallery/image-11.jpg`}/>
+                                </div>
                             </div>
                         </div>
                     </div>
